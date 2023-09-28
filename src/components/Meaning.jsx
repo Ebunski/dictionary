4cb907cd646e614bca6ef7231749157b32c8ef47
Catalyst <danielduyilemi@gmail.com>
2023-09-28 10:58:08 +0100

    ádd other components

---
+++ src/components/Meaning.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+function Meaning() {
+  return (
+    <section className="meaning">
+      <div className="noun py-6">
+        <div className="title flex gap-4 items-center">
+          <div className="font-bold text-[1.5rem] italic">noun</div>
+          <div className="hl flex-1 border-t-[1px] border-t-[#f4f4f4]"></div>
+        </div>
+        <div className="text-[#84849a] py-6">Meaning</div>
+        <ul className="list-disc pl-5">
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+        </ul>
+        <div className="extras py-3">
+            <div className="py-2">
+                <span className="name text-[#84849a]">Synonyms: </span>
+                <span className="text-[#9e35e9]">frith.</span>
+            </div>
+            <div className="py-2">
+                <span className="name text-[#84849a]">Antonyms: </span>
+                <span className="text-[#9e35e9]">disruption, violence, war</span>
+            </div>
+        </div>
+      </div>
+      <div className="verb py-6">
+        <div className="title flex gap-4 items-center">
+          <div className="font-bold text-[1.5rem] italic">verb</div>
+          <div className="hl flex-1 border-t-[1px] border-t-[#f4f4f4]"></div>
+        </div>
+        <div className="text-[#84849a] py-6">Meaning</div>
+        <ul className="list-disc pl-5">
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+        </ul>
+      </div>
+      <div className="interjection py-6">
+        <div className="title flex gap-4 items-center">
+          <div className="font-bold text-[1.5rem] italic">interjection</div>
+          <div className="hl flex-1 border-t-[1px] border-t-[#f4f4f4]"></div>
+        </div>
+        <div className="text-[#84849a] py-6">Meaning</div>
+        <ul className="list-disc pl-5">
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+            <li className="">
+                <div className="pb-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat nihil harum veritatis iure nesciunt incidunt consectetur dolores, aut quia tenetur ratione, dolor vitae reprehenderit?</div>
+                <div className="pt-2 text-[#84849a]">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam quas sequi a, provident delectus laudantium necessitatibus quo molestiae mollitia."</div>
+            </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Meaning;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="flex items-center justify-between py-8 md:py-[3rem]">
+      <img src="./assets/dictionary-logo.svg" alt="Book" />
+      <div className="options flex gap-4 items-center">
+        <div className="font-select flex items-center gap-4 px-6 border-e-[1px] border-e-[#e9e9e9]">
+          <div>Serif</div>
+          <div className="w-8">
+            <img src="./assets/expand_more.png" alt="Expand More" />
+          </div>
+        </div>
+        <div className="flex gap-4 items-center">
+          <input type="checkbox" />
+          <button>
+            <img src="./assets/moon.svg" alt="" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
