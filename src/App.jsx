c302a6a915a44f1108cec1e7631b03dfd05cae15
Ebun <oyebolaebun07@gmail.com>
2023-09-29 23:26:38 +0100

    displayed suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useSelector } from "react-redux";
 import Meanings from "./components/Meanings";
 import Nav from "./components/Nav";
 import SearchBar from "./components/SearchBar";
+import Sug from "./components/Suggestions";
 import Word from "./components/Word";
 const App = () => {
-  const {darkMode} = useSelector(st => st.user)
-  const background = darkMode ?  "bg-[#050505]": "bg-[#ffffff]";
+  const { darkMode } = useSelector((st) => st.user);
+  const background = darkMode ? "bg-[#050505]" : "bg-[#ffffff]";
   const textMain = darkMode ? "text-[#ffffff]" : "text-[#2d2d2d]";
   return (
-    <div className={`App flex justify-center font-lora ${background} ${textMain}`}>
+    <div
+      className={`App flex justify-center font-lora ${background} ${textMain}`}
+    >
       <div className="w-[95%] md:w-[60%]">
         <Nav />
         <SearchBar />
+        <Sug />
         <Word />
         <Meanings />
       </div>
