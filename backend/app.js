3924193aab50b2c537b74a8de3b3767bb414f059
Ebun <oyebolaebun07@gmail.com>
2023-10-11 14:11:26 +0100

    solve env issue jare

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require( 'dotenv' ).config();
-require('./helpers/init_mongodb');
-app.get('/', (req,res) => {
-    res.send('Hello World. Backend is connected.')
-})
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, (err) => err ? console.log(err) : console.log('Plus Ultra') )
+require("dotenv").config();
+require("./helpers/init_mongodb");
+app.get("/", (req, res) => {
+  res.send("Hello World. Backend is connected.");
+});
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, (err) => (err ? console.log(err) : console.log("Plus Ultra")));
